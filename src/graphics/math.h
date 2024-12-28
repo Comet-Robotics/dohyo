32849e6745313d78e0f2236988cecc90ad6aa6f7
Nathan <njw230004@utdallas.edu>
2024-12-27 21:34:40 -0600

    Box2D debug rendering

---
+++ src/graphics/math.h
@@ -0,0 +1,10 @@
+#ifndef MATH_H
+#define MATH_H
+
+#include <box2d/box2d.h>
+#include <SDL3/SDL.h>
+
+#endif //MATH_H
+
+void cartesianToScreen(float* x, float* y);
+void hexColorToRGB(b2HexColor hexColor, SDL_FColor* color);
